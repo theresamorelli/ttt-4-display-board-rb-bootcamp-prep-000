bfa3c19b5ea6f5af6fe1bbd0fbe5ef60786f08ab
Theresa <theresa.l.morelli@gmail.com>
2018-05-07 23:06:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,4 +6,9 @@ def display_board(board)
   line_1 = " #{board[0]} | #{board[1]} | #{board[2]} "
   line_2 = " #{board[3]} | #{board[4]} | #{board[5]} "
   line_3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts line_1
+  puts h_line
+  puts line_2
+  puts h_line
+  puts line_3
 end
